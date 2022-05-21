af165403ddf3c0cf9e6b364802ac4ca2a08850f4
Nam <ptnnamaxe@gmail.com>
2022-05-21 21:07:24 +0700

    video 23

---
+++ src/seeders/seeder-user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'hidanIT',
+      lastName: 'Eric',
+      address: 'Ha Noi',
+      gender: '1',
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+  }
+};
+// email: DataTypes.STRING,
+// password: DataTypes.STRING,
+// firstName: DataTypes.STRING,
+// lastName: DataTypes.STRING,
+// address: DataTypes.STRING,
+// gender: DataTypes.BOOLEAN,
+// roleid: DataTypes.STRING,
+
