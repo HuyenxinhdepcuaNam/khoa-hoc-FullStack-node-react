1ad81f3617780cf72a3e8ff0af8c9d8112955c2b
Nam <ptnnamaxe@gmail.com>
2022-06-09 20:29:10 +0700

    video 67

--- src/models/markdown.js
+++ src/models/markdown.js
@@ -3,7 +3,7 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Mardown extends Model {
+    class Markdown extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     };
-    Mardown.init({
+    Markdown.init({
         contentHTML: DataTypes.TEXT('long'),
         contentMarkdown: DataTypes.TEXT('long'),
         description: DataTypes.TEXT('long'),
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
 
     }, {
         sequelize,
-        modelName: 'Mardown',
+        modelName: 'Markdown',
     });
-    return Mardown;
+    return Markdown;
 };
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,3 +1,4 @@
+import { reject } from 'bcrypt/promises'
 import db from '../models/index'
 
 
@@ -29,6 +30,53 @@ let getTopDoctorHomeService = (limitInput) => {
     })
 }
 
+let getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where: { roleId: 'R2' },
+                attributes: {
+                    exclude: ['image', 'password']
+                }
+            })
+            resolve({
+                errCode: 0,
+                data: doctors
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let saveDetailInfoDoctor = (inputData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing input!'
+                })
+            } else {
+                await db.Markdown.create({
+                    contentHTML: inputData.contentHTML,
+                    contentMarkdown: inputData.contentMarkdown,
+                    description: inputData.description,
+                    doctorId: inputData.doctorId
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: 'Save info doctor!'
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    getTopDoctorHomeService: getTopDoctorHomeService
+    getTopDoctorHomeService: getTopDoctorHomeService,
+    getAllDoctors: getAllDoctors,
+    saveDetailInfoDoctor: saveDetailInfoDoctor
 }
