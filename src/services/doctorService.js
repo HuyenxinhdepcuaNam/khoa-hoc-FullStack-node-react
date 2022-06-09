5feeac26d1f747459eb00ae81489ce1b889e0f73
Nam <ptnnamaxe@gmail.com>
2022-06-10 06:13:25 +0700

    video 68

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -75,8 +75,49 @@ let saveDetailInfoDoctor = (inputData) => {
     })
 }
 
+let getDetailDoctorById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing input!'
+                })
+            } else {
+                let data = await db.User.findOne({
+                    where: { id: inputId },
+                    attributes: {
+                        exclude: ['password', 'image']
+                    },
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: ['contentHTML', 'contentMarkdown', 'description']
+                        },
+                        {
+                            model: db.Allcode, as: 'positionData',
+                            attributes: ['valueEn', 'valueVi']
+                        },
+
+                    ],
+                    raw: true,
+                    nest: true
+                })
+                resolve({
+                    errCode: 0,
+                    data: data
+                })
+            }
+
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHomeService: getTopDoctorHomeService,
     getAllDoctors: getAllDoctors,
-    saveDetailInfoDoctor: saveDetailInfoDoctor
+    saveDetailInfoDoctor: saveDetailInfoDoctor,
+    getDetailDoctorById: getDetailDoctorById
 }
