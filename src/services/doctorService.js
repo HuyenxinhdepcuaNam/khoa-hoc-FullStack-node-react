0d1591a95c8f2f13420be7dd6c462c041625d524
Nam <ptnnamaxe@gmail.com>
2022-06-25 15:27:33 +0700

    video 92

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -51,18 +51,35 @@ let getAllDoctors = () => {
         }
     })
 }
+let checkRequiredFields = (inputData) => {
+    let arrFields = [
+        'doctorId', 'contentHTML', 'contentMarkdown', 'action', 'selectedPrice',
+        'selectedPayment', 'selectedProvince', 'nameClinic', 'addressClinic',
+        'note', 'specialtyId'
+    ]
+    let isValid = true
+    let element = ''
+    for (let i = 0; i < arrFields.length; i++) {
+        if (!inputData[arrFields[i]]) {
+            isValid = false
+            element = arrFields[i]
+            break
+        }
+    }
+    return {
+        isValid: isValid,
+        element: element
+    }
+}
 
 let saveDetailInfoDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!inputData.doctorId || !inputData.contentHTML
-                || !inputData.contentMarkdown || !inputData.action
-                || !inputData.selectedPrice || !inputData.selectedPayment
-                || !inputData.selectedProvince || !inputData.nameClinic
-                || !inputData.addressClinic || !inputData.note) {
+            let checkObj = checkRequiredFields(inputData)
+            if (checkObj.isValid === false) {
                 resolve({
                     errCode: 1,
-                    errMessage: 'Missing input!'
+                    errMessage: `Missing ${checkObj.element}`
                 })
             } else {
                 // upsert infor markdown
@@ -100,6 +117,8 @@ let saveDetailInfoDoctor = (inputData) => {
                     doctorInfor.addressClinic = inputData.addressClinic
                     doctorInfor.nameClinic = inputData.nameClinic
                     doctorInfor.note = inputData.note
+                    doctorInfor.specialtyId = inputData.specialtyId
+                    doctorInfor.clinicId = inputData.clinicId
                     await doctorInfor.save()
                 } else {
                     // create
@@ -110,7 +129,10 @@ let saveDetailInfoDoctor = (inputData) => {
                         provinceId: inputData.selectedProvince,
                         addressClinic: inputData.addressClinic,
                         nameClinic: inputData.nameClinic,
-                        note: inputData.note
+                        note: inputData.note,
+                        specialtyId: inputData.specialtyId,
+                        clinicId: inputData.clinicId
+
                     })
                 }
                 resolve({
