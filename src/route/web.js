bec7c65a937d715c227053a2c342123ea15a2baf
Nam <ptnnamaxe@gmail.com>
2022-06-30 20:54:04 +0700

    video 100

--- src/route/web.js
+++ src/route/web.js
@@ -36,7 +36,9 @@ let initWebRoutes = (app) => {
     router.get('/api/get-schedule-by-date', doctorController.getScheduleByDate)
     router.get('/api/get-extra-infor-by-id', doctorController.getExtraInforById)
     router.get('/api/get-profile-doctor-by-id', doctorController.getProfileDoctorById)
+
     router.get('/api/get-list-patient-for-doctor', doctorController.getListPatientForDoctor)
+    router.post('/api/send-remedy', doctorController.sendRemedy)
 
 
 
--- src/services/emailService.js
+++ src/services/emailService.js
@@ -58,6 +58,60 @@ let getBodyHTMLEmail = (dataSend) => {
     return result
 }
 
+let sendAttachment = async (dataSend) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_APP, // generated ethereal user
+            pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Dota 2 with us 👻" <diia0chuidit@gmail.com>', // sender address
+        to: dataSend.email, // list of receivers
+        subject: "Kết quả đặt lịch khám bệnh ✔", // Subject line
+        // text: "Hello world?", // plain text body
+        html: getBodyHTMLEmailRemedy(dataSend),// html body
+        attachments: [
+            {
+                filename: 'text1.png',
+                content: dataSend.imgBase64.split('base64,')[1],
+                encoding: 'base64',
+
+            },
+        ],
+
+
+    });
+}
+
+let getBodyHTMLEmailRemedy = (dataSend) => {
+    let result = ''
+    if (dataSend.language === 'vi') {
+        result =
+            `
+        <h3>Xin chào ${dataSend.patientName} </h3>
+        <p>Thông tin đặt lịch khám bệnh:</p>
+        <p>Xác nhận để hoàn tất thủ tục đặt lịch</p>
+        `
+    }
+    if (dataSend.language === 'en') {
+        result =
+            `
+        <h3>Dear ${dataSend.patientName} </h3>
+        <p>Booking information:</p>
+        <p>Confirm and complete!</p>
+        `
+
+    }
+    return result
+}
+
 module.exports = {
-    sendSimpleEmail,
+    sendSimpleEmail, sendAttachment
 }
